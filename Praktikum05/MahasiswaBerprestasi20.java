b5677e903de9a4634f42ce2baf37d8e9c2cda25a
ratnawahyuni12 <ratna120307@gmail.com>
2026-04-01 10:57:00 +0700

    5.3 Praktikum 2 –– Bubble Sort (MahasiswaBerprestasi20.java)

---
+++ Praktikum05/MahasiswaBerprestasi20.java
@@ -0,0 +1,32 @@
+public class MahasiswaBerprestasi20 {
+    Mahasiswa20 [] listMhs = new Mahasiswa20 [5];
+    int idx;
+
+    void tambah (Mahasiswa20 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa20 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("----------------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa20 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
