139d671cd30bbc1b05f53ed46316a036c8495342
ratnawahyuni12 <ratna120307@gmail.com>
2026-04-01 11:59:08 +0700

    5.3 Praktikum 2 –– Modifikasi Pertanyaan (Insertion Sort) –– MahasiswaBerprestasi20.java

--- Praktikum05/MahasiswaBerprestasi20.java
+++ Praktikum05/MahasiswaBerprestasi20.java
@@ -48,7 +48,7 @@ public class MahasiswaBerprestasi20 {
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa20 temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) { // > diganti
                 listMhs[j] = listMhs[j - 1];
                 j--;
             }
