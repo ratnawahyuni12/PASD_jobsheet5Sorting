a6a0053bbfe97d3f1a790de711194b7fb48801f7
ratnawahyuni12 <ratna120307@gmail.com>
2026-04-01 10:06:15 +0700

    5.2 Praktikum 1 –– Selection Sort (SortingMain20.java)

--- Praktikum05/SortingMain20.java
+++ Praktikum05/SortingMain20.java
@@ -1,6 +1,7 @@
 public class SortingMain20 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
 
         Sorting20 dataurut1 = new Sorting20(a, a.length);
         System.out.println("Data awal 1");
@@ -8,5 +9,12 @@ public class SortingMain20 {
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        Sorting20 dataurut2 = new Sorting20(b, b.length);
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }
